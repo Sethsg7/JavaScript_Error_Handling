5314a55540d732ff6b97173afffd03b9ace00b9e
Seth S. Green <sethsg7@gmail.com>
2023-01-18 17:22:46 -0600

    ErrorHandlingWork

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // TODO: Enable strict mode
-
+"use strict";
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+  try {
+    JSON.parse(data);
+  } catch (err) {
+    //If err occurrs, it auto becomes parameter.
+
+    console.error(err);
+    //Use console.error to make the err msg red.
+
+    //Log the error then return null.
+
+    return null;
+  }
+  //Try/Catch allows the code to continue running.
+
+  return data;
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
-let passData = JSON.stringify({ success: true });
-
+let passData = JSON.stringify({ hello: true });
+console.log(passData);
 console.log(parseToJSON(failData));
 
 console.log(parseToJSON(passData));
